253edffb14b10470248ef61e3a52a53a6ac382ac
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-03 14:43:47 -0300

    Add props.children to PageHeader and modify styles to add responsiveness

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -23,6 +23,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 			</div>
 			<div className="header-content">
 				<strong>{props.title}</strong>
+				{props.children}
 			</div>
 		</header>
 	);
