de8beef33ff4afb017d7f95e525e1bcc0625e8b2
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-04 15:22:42 -0300

    feature(server): create method to list classes

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -11,6 +11,43 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+	// Padrão mvc listagem, se chama index
+	async index(request: Request, response: Response) {
+		const filters = request.query;
+
+		const subject = filters.subject as string;
+		const week_day = filters.week_day as string;
+		const time = filters.time as string;
+
+		if (!filters.week_day || !filters.subject || !filters.time) {
+			return response.status(400).json({
+				error: 'Missing filters to search classes',
+			});
+		}
+
+		// dizer pro TypeScript que o time é uma string
+		const timeInMinutes = convertHourToMinutes(time);
+
+		// Inner join, trazendo os dados da classe e do usuário
+		const classes = await database('classes')
+			// Subquery, só retorna caso exista um registro atendendo as condições
+			.whereExists(function () {
+				this.select('class_schedule.*')
+					.from('class_schedule')
+					.whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+					.whereRaw('`class_schedule`.`week_day` = ??', [
+						Number(week_day),
+					])
+					.whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+					.whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
+			})
+			.where('classes.subject', '=', subject)
+			.join('users', 'classes.user_id', '=', 'users.id')
+			.select(['classes.*', 'users.*']);
+
+		return response.json(classes);
+	}
+
 	async create(request: Request, response: Response) {
 		// Desestruturação
 		const {
