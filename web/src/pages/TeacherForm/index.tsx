42b13d32401571cacf9edf8f6bf26b7d6193bd61
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-05 12:23:28 -0300

    feature(web): create form to give classes

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
 
@@ -11,6 +14,30 @@ function TeacherForm() {
 				title="Que incrível que você quer dar aulas."
 				description="O primeiro passo é preencher esse formulário de inscrição"
 			/>
+
+			<main>
+				<fieldset>
+					<legend>Seus dados</legend>
+					<Input name="name" label="Nome completo" />
+					<Input name="avatar" label="Avatar" />
+					<Input name="whatsapp" label="Whatsapp" />
+				</fieldset>
+
+				<fieldset>
+					<legend>Sobre a aula</legend>
+					<Input name="subject" label="Matéria" />
+					<Input name="cost" label="Custo da sua hora por aula" />
+				</fieldset>
+
+				<footer>
+					<p>
+						<img src={warningIcon} alt="Aviso importante" />
+						Importante! <br />
+						Preencha todos os dados
+					</p>
+					<button type="button">Salvar cadastro</button>
+				</footer>
+			</main>
 		</div>
 	);
 }
