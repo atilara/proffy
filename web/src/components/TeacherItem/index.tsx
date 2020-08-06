ed4f29fbfb000b2d22754d202fec86de0f49e421
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-06 11:31:02 -0300

    feature(web): add support to list teachers

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,39 +3,57 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+	avatar: string;
+	bio: string;
+	cost: number;
+	id: number;
+	name: string;
+	subject: string;
+	whatsapp: string;
+}
+
+interface TeacherItemProps {
+	teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({
+	teacher,
+}) => {
+	function createNewConnection() {
+		api.post('connections', {
+			user_id: teacher.id,
+		});
+	}
+
 	return (
 		<article className="teacher-item">
 			<header>
-				<img
-					src="https://avatars1.githubusercontent.com/u/45675035?s=460&u=e8277ab9d3eb18151613a2699e1fbb6572a3dcc4&v=4"
-					alt="Átila Rodrigues"
-				/>
+				<img src={teacher.avatar} alt={teacher.name} />
 				<div>
-					<strong>Átila Rodrigues</strong>
-					<span>Física</span>
+					<strong>{teacher.name}</strong>
+					<span>{teacher.subject}</span>
 				</div>
 			</header>
-			<p>
-				Entusiasta pelas melhores tecnologias de física avançada
-				<br />
-				<br />
-				Apaixonado por explodir coisas em demonstrações e por mudar a
-				vida das pessoas através de experiências.
-			</p>
+			<p>{teacher.bio}</p>
 			<footer>
 				<p>
 					Preço/hora
-					<strong>R$ 50,00</strong>
+					<strong>R$ {teacher.cost}</strong>
 				</p>
-				<button type="button">
+				<a
+					target="_blank"
+					onClick={createNewConnection}
+					href={`https://wa.me/${teacher.whatsapp}`}
+				>
 					<img src={whatsappIcon} alt="Whatsapp" />
 					Entrar em contato
-				</button>
+				</a>
 			</footer>
 		</article>
 	);
-}
+};
 
 export default TeacherItem;
