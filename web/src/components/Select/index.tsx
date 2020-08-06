59131c82ec78136909157173a7f210bc622c7f92
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-06 09:24:58 -0300

    fix(web): avoid warning using the correct prop

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -17,8 +17,8 @@ const Select: React.FC<SelectProps> = ({ name, label, options, ...rest }) => {
 	return (
 		<div className="select-block">
 			<label htmlFor={name}>{label}</label>
-			<select id={name} {...rest}>
-				<option value="" disabled selected hidden>
+			<select defaultValue="" id={name} {...rest}>
+				<option value="" disabled hidden>
 					Selecione uma opção
 				</option>
 				{options.map((option) => {
