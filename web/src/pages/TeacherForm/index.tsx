d6e27b523f4562b278397cfcac229bce31a21a66
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-05 21:04:29 -0300

    feature(web): create fieldset to schedule classes

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -50,6 +50,31 @@ function TeacherForm() {
 					<Input name="cost" label="Custo da sua hora por aula" />
 				</fieldset>
 
+				<fieldset>
+					<legend>
+						Horários disponíveis
+						<button type="button">+ Novo horário</button>
+					</legend>
+
+					<div className="schedule-item">
+						<Select
+							name="week_day"
+							label="Dia da semana"
+							options={[
+								{ value: '0', label: 'Domingo' },
+								{ value: '1', label: 'Segunda-feira' },
+								{ value: '2', label: 'Terça-feira' },
+								{ value: '3', label: 'Quarta-feira' },
+								{ value: '4', label: 'Quinta-feira' },
+								{ value: '5', label: 'Sexta-feira' },
+								{ value: '6', label: 'Sábado' },
+							]}
+						/>
+						<Input name="from" label="Das" type="time" />
+						<Input name="to" label="Até" type="time" />
+					</div>
+				</fieldset>
+
 				<footer>
 					<p>
 						<img src={warningIcon} alt="Aviso importante" />
