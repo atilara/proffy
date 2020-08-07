3a213d7a1bd90af657bcb0ff2e481ac27d4030d3
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-07 12:44:28 -0300

    feature(mobile): create Landing page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { AppLoading } from 'expo';
+
+import {
+	Archivo_400Regular,
+	Archivo_700Bold,
+	useFonts,
+} from '@expo-google-fonts/archivo';
+
+import {
+	Poppins_400Regular,
+	Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+
+import Landing from './src/pages/Landing';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
-}
+	let [fontsLoaded] = useFonts({
+		Archivo_400Regular,
+		Archivo_700Bold,
+		Poppins_400Regular,
+		Poppins_600SemiBold,
+	});
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	} else {
+		return (
+			<>
+				<Landing />
+				<StatusBar style="light" />
+			</>
+		);
+	}
+}
